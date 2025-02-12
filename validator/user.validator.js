d49a4897a38ccb6acb978b6b0e7c21e227464b50
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-02-12 22:09:05 +0530

    [Sparsh] Refactor: Password Validation Updated with Constraints

--- validator/user.validator.js
+++ validator/user.validator.js
@@ -4,7 +4,15 @@ export const newUserValidator = (req, res, next) => {
     const userSchema = Joi.object({
         name: Joi.string().min(3).max(30).required(),
         email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
+        password: Joi.string()
+            .pattern(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{6,}$/
+            )
+            .required()
+            .messages({
+                'string.pattern.base':
+                    'Password must be at least 6 characters long, and include at least one uppercase letter, one lowercase letter, one number, and one special character.'
+            }),
         phone: Joi.string().pattern(/^[0-9]{10}$/).required().messages({ 'string.pattern.base': 'Phone Number must be Exactly 10 Digits' }),
         role: Joi.string().valid('owner', 'admin').default('owner')
     });
